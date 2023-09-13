d494a19e5cc8cda098cd49293ec18ba1f0e4adf4
Oscar Rodriguez Hernandez <91960921+oscarrhdatascience@users.noreply.github.com>
2023-09-13 10:27:08 +0200

    Create 5_FibonacciList.py

---
+++ 5_FibonacciList.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+print(fibonacci(5))  # Output: [0, 1, 1, 2, 3]
