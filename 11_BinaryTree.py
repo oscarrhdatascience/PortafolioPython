11eed940a5a0331f3873a8eddb626b2e767ae5b1
Oscar Rodriguez Hernandez <91960921+oscarrhdatascience@users.noreply.github.com>
2023-09-13 10:48:22 +0200

    Create 11_BinaryTree.py

---
+++ 11_BinaryTree.py
@@ -0,0 +1,52 @@
+public class BinaryTree {
+    Node root;
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.data) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.data) {
+            root.right = insertRec(root.right, key);
+        }
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.data);
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(40);
+        tree.insert(60);
+        tree.inorder();  // 30 40 50 60 70
+    }
+}
